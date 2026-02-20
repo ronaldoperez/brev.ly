818971fd7a876e4b081c97a2db27683f235a6b79
Ronaldo Perez <ronaldofperez@gmail.com>
2026-02-20 08:42:10 -0300

    adicionado postgres, drizzle e uuidv7

---
+++ server/src/infra/db/schemas/urls.ts
@@ -0,0 +1,12 @@
+import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+import { uuidv7 } from 'uuidv7'
+
+export const uploads = pgTable('urls', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => uuidv7()),
+  originalUrl: text('original_url').notNull(),
+  shortCode: text('short_code').notNull().unique(),
+  clicks: integer('clicks').notNull().default(0),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+})
--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,7 +1,29 @@
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
 const server = fastify()
+
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation Error',
+      issues: error.validation,
+    })
+  }
+
+  //envia erro para uma ferramenta de observalidade Sentry/Datadog/Grafana/Otel
+  console.error(error)
+
+  return reply.status(500).send({ message: 'Internal server Error.' })
+})
 
 server.register(fastifyCors, {
   origin: '*',
